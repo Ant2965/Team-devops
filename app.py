9a87de580546efcf4a283c07f87f9803cf5a2933
Antriksh <Antriksh>
2023-07-13 07:57:13 +0530

    some html

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 import psutil
 from flask import Flask, render_template
-from prometheus_client import CollectorRegistry, Guage, push_to_gateway
+#from prometheus_client import CollectorRegistry, Guage, push_to_gateway
 app = Flask(__name__)
-registry=CollectorRegistry()
-mem=Guage('mem_metric','last time',registry=registry)
-cpu=Guage('cpu_metric','last time',registry=registry)
-cpu.set_to_current_time()
-mem.set_to_current_time()
-
 
 @app.route("/")
 def index():
     cpu_metric = psutil.cpu_percent()
     mem_metric = psutil.virtual_memory().percent
-    mem.set(mem_metric)
-    cpu.set(cpu_metric)
-    push_to_gateway('prom-prometheus-pushgateway:80',job='batchA',registry=registry)
 
     Message = None
     if cpu_metric > 80 or mem_metric > 80:
